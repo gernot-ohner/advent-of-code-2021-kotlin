d26a38fdd3edc23e03faf7c3fc7175b9bc93667a
gernot-ohner <gernot.ohner@gmail.com>
2021-12-01 02:12:18 -0500

    Day 1, part 1 & 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input
+        .map { it.toInt() }
+        .windowed(2)
+        .count { (i1, i2) -> i2 > i1 }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int = input
+        .map { it.toInt() }
+        .windowed(3, transform = { it.sum() })
+        .windowed(2)
+        .count { (i1, i2) -> i2 > i1 }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("1", "test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("1", "prod")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,7 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(day: String, testOrProd: String) = File("inputs/day$day", "$testOrProd.txt").readLines()
 
 /**
  * Converts string to md5 hash.
