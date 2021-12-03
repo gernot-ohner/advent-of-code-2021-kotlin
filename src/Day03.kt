1c511a2fc5c7753225ff7c75ae3996159a2cfcf0
gernot-ohner <gernot.ohner@gmail.com>
2021-12-03 03:30:56 -0500

    Day 3, part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,41 +1,46 @@
-// there's probably an O(1) space solution
-// but for now I'm going to go with the O(n)
 fun part1(input: List<String>): Int {
-    val lineLength = input.first().length
-    val rowNum = input.size
-    val initialCount = List(lineLength) { 0 }
-    val oneCounts = input.fold(initialCount) { l, s ->
-        s.toCharArray()
-            .map { toInt(it) }
-            .zip(l)
-            .map { (i1, i2) -> i1 + i2 }
-    }
-    val majorityElements = oneCounts
-        .map { it > rowNum / 2 }
-    val gamma = majorityElements
-        .map { it.toChar() }
-        .joinToString("")
-        .toInt(2)
-    val epsilon = majorityElements
-        .map { (!it).toChar() }
-        .joinToString("", "", "")
-        .toInt(2)
-    println(gamma)
-    println(epsilon)
+    val elementLength = input.first().length
+    val chars = (0 until elementLength)
+        .map { i -> findMajority(input.map { it[i] }) }
+    val gamma = chars
+        .toBinaryInt()
+    val epsilon = chars
+        .map { it.reverseZeroAndOne() }
+        .toBinaryInt()
     return gamma * epsilon
 }
 
+fun part2(input: List<String>): Int {
+    val oxygen = rec(0, input, false)
+    val co2 = rec(0, input, true)
+    return oxygen * co2
+}
 
-fun Boolean.toChar(): Char = ((if (this) 1 else 0) + '0'.code).toChar()
-fun toInt(c: Char): Int = c.code - '0'.code
+fun rec(i: Int, input: List<String>, carbon: Boolean): Int =
+    if (i >= input.first().length || input.size == 1)
+        input.first().toInt(2)
+    else {
+        val majority = findMajority(input.map { it[i] })
+        rec(i + 1, input.filter { (it[i] == majority) xor carbon }, carbon)
+    }
 
+fun List<Char>.toBinaryInt(): Int =
+    this.joinToString("").toInt(2)
+
+fun Char.reverseZeroAndOne(): Char =
+    if (this == '0') '1'
+    else if (this == '1') '0'
+    else this
+
+fun findMajority(chars: List<Char>): Char =
+    if (chars.count { it == '1' } >= chars.size / 2.0) '1' else '0'
 
 fun main() {
     val testInput = readInput("3", "test")
     check(part1(testInput) == 198)
-//    check(Day2Part2.compute(testInput) == 900)
+    check(part2(testInput) == 230)
 
     val input = readInput("3", "prod")
     println(part1(input))
-//    println(Day2Part2.compute(input))
+    println(part2(input))
 }
